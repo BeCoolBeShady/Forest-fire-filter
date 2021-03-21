b061a6965463333c31a4dce2b1ff9be60e201ae8
Benedykt Cieslinski <39050190+BeCoolBeShady@users.noreply.github.com>
2021-03-21 19:44:45 +0000

    Added the project

---
+++ comp120-tinkering-graphics/Forest Fires and Fake News/Project Files/Forest Fires and Fake News/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Forest_Fires_and_Fake_News
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox8 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(538, 786);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(472, 22);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button2.Location = new System.Drawing.Point(729, 814);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseWaitCursor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(679, 758);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Search Image by Directory:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1036, 789);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 17);
+            this.label2.TabIndex = 13;
+            // 
+            // pictureBox8
+            // 
+            this.pictureBox8.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pictureBox8.BackgroundImage = global::Forest_Fires_and_Fake_News.Properties.Resources.Arrow;
+            this.pictureBox8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox8.Location = new System.Drawing.Point(692, 349);
+            this.pictureBox8.Name = "pictureBox8";
+            this.pictureBox8.Size = new System.Drawing.Size(153, 94);
+            this.pictureBox8.TabIndex = 11;
+            this.pictureBox8.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pictureBox1.Location = new System.Drawing.Point(875, 222);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(618, 511);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pictureBox3.Location = new System.Drawing.Point(34, 222);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(623, 511);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pictureBox6.BackgroundImage = global::Forest_Fires_and_Fake_News.Properties.Resources.OriginalBox;
+            this.pictureBox6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox6.Location = new System.Drawing.Point(12, 202);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(669, 550);
+            this.pictureBox6.TabIndex = 9;
+            this.pictureBox6.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.button1.BackColor = System.Drawing.Color.Peru;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(215, 758);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 56);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pictureBox5.BackgroundImage = global::Forest_Fires_and_Fake_News.Properties.Resources.After;
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox5.Location = new System.Drawing.Point(1118, 157);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(204, 39);
+            this.pictureBox5.TabIndex = 4;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.pictureBox4.BackgroundImage = global::Forest_Fires_and_Fake_News.Properties.Resources.Before;
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox4.Location = new System.Drawing.Point(229, 157);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(204, 39);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.pictureBox2.BackgroundImage = global::Forest_Fires_and_Fake_News.Properties.Resources.Title;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(178, 28);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1186, 124);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.pictureBox7.BackgroundImage = global::Forest_Fires_and_Fake_News.Properties.Resources.OriginalBox;
+            this.pictureBox7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox7.Location = new System.Drawing.Point(851, 201);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(669, 550);
+            this.pictureBox7.TabIndex = 10;
+            this.pictureBox7.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1545, 895);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox7);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Forest Fire Defilter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox pictureBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ comp120-tinkering-graphics/Forest Fires and Fake News/Project Files/Forest Fires and Fake News/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forest_Fires_and_Fake_News
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Bitmap bmp;
+        bool hasImagedChanged = false; //This variable stops the user from pressing the Fix button more then once
+        public void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        // Takes the RGB value of the pixel and reverses it
+        public void Invert()
+        {
+            for (int y = 0; y < bmp.Height; y++)
+            {
+                for (int x = 0; x < bmp.Width; x++)
+                {
+                    Color p = bmp.GetPixel(x, y);
+                    float R = p.R;
+                    float G = p.G;
+                    float B = p.B;
+
+                    float temp;
+
+                    //Swapping R to B and B to R
+                    temp = R;
+                    R = B;
+                    B = temp;
+
+                    bmp.SetPixel(x, y, Color.FromArgb(255, (int)R, (int)G, (int)B));
+                     
+                }
+            }
+        }
+
+        //Reduces Orange colour from a picture
+        public void ReduceOrange()
+        {
+            if (bmp != null) //If the player has searched for an image 
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    for (int x = 0; x < bmp.Width; x++)
+                    {
+                        Color p = bmp.GetPixel(x, y);
+                        float R = p.R;
+                        float G = p.G;
+                        float B = p.B;
+                        Color red = Color.FromArgb(255, 255, 0, 0);
+
+                        //Changing pixels which colour is close to red with tolerance of 220, just to be sure most pixels will be changed
+                        if (Tolerance(p, red, 220))
+                        {
+                            R -= Luminance(p);
+                            G += 10;
+                            B += (Luminance(p) / 2);
+                        }
+
+                        //The IF's are to prevent the values to go over/under the max/min value of RGB 
+                        if (G > 255)
+                            G = 255;
+                        if (B > 255)
+                            B = 255;
+                        if (R < 0)
+                            R = 0;
+
+                        bmp.SetPixel(x, y, Color.FromArgb(255, abs((int)R), abs((int)G), abs((int)B)));
+                    }
+                }
+                MakeBrighter(1.2f);
+                Invert();
+            }
+            else //if there is no image then display this text in label 2
+            {
+                label2.Text = "There is no Image! Please Search for an Image";
+            }
+        }
+        
+        //Makes the image brighter by the set float value
+        private void MakeBrighter(float tmp)
+        {
+            for (int x = 0; x < bmp.Width; x++)
+            {
+                for (int y = 0; y < bmp.Height; y++)
+                {
+                    Color c1 = bmp.GetPixel(x, y);
+                    float R = c1.R;
+                    float G = c1.G;
+                    float B = c1.B;
+                    R *= tmp;
+                    G *= tmp;
+                    B *= tmp;
+
+                    //IF's prevent the values to go over the max value of RGB
+                    if (G > 255)
+                        G = 255;
+                    if (B > 255)
+                        B = 255;
+                    if (R > 255)
+                        R = 255;
+
+                    bmp.SetPixel(x, y, Color.FromArgb(255, abs((int)R), abs((int)G), abs((int)B)));
+                }
+            }
+        }
+
+        //Returns an absolute value
+        private int abs(int x)
+        {
+            if (x > 0)
+                return x;
+            else
+                return -x;
+        }
+
+        //Calculates the distance between two colours
+        private float ColourDistance(Color color, Color color2)
+        {
+
+            float distance;
+
+            float math = (float)Math.Pow(color.R - color2.R, 2) + (float)Math.Pow(color.G - color2.G, 2) + (float)Math.Pow(color.B - color2.B, 2);
+
+            distance = (float)Math.Sqrt(math);
+
+            return distance;
+        }
+
+        //Calculates the Luminance of a pixel
+        private int Luminance(Color c1)
+        {
+            int luminance = (c1.R + c1.G + c1.B) / 3;
+
+            return luminance;
+        }
+
+        //Checks if the Distance between two colours is bigger or smaller than the set tolerance value
+        private bool Tolerance(Color c1, Color c2, int t) 
+        {
+            return ColourDistance(c1, c2) < t;
+        }
+
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (hasImagedChanged == false)
+            {
+                hasImagedChanged = true;
+                //Starts the reduce orange function
+                pictureBox1.Image = bmp;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                ReduceOrange();
+            }
+            else
+            {
+                label2.Text = "DONT PRESS THE FIX IMAGE BUTTON MORE THEN ONCE!";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String Input = textBox1.Text;
+            try
+            {
+                // open file dialog   
+                OpenFileDialog open = new OpenFileDialog();
+                // image filters  
+                open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+                if (open.ShowDialog() == DialogResult.OK)
+                {
+                    // display image in picture box  
+                    pictureBox3.Image = new Bitmap(open.FileName);
+                    // image file path  
+                    textBox1.Text = open.FileName;
+                    bmp = new Bitmap(open.FileName);
+                }
+                pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+                label2.Text = "";
+                hasImagedChanged = false;
+            }
+            catch
+            {
+                label2.Text = "Failed to Find Image! Remeber to use double slashes e.g \\ and make sure the Image exists!";
+            }
+
+        }
+    }
+}
